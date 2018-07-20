cd8d81c5a6aea56c1f0c736a87acaec036eaccf9
ands <ands@users.noreply.github.com>
2018-07-20 20:41:43 +0200

    Fixed menu entry hotkeys

--- this_is_fine_menu.h
+++ this_is_fine_menu.h
@@ -5,9 +5,9 @@ void toggle(struct tray_menu *item) { item->checked = !item->checked; tray_updat
 void quit(struct tray_menu *item) { tray_exit(); ExitProcess(0); }
 #endif
 
-ENTRY("Alt + Q | Explorer: Tray", explorer_tray, MOD_ALT | MOD_NOREPEAT, 'Q')
+ENTRY("Alt + E | Explorer: Tray", explorer_tray, MOD_ALT | MOD_NOREPEAT, 'E')
 SEPARATOR
 ENTRY("Alt + M | Build"         , build        , MOD_ALT | MOD_NOREPEAT, 'M')
 ENTRY("Alt + T | Toggle Example", toggle       , MOD_ALT | MOD_NOREPEAT, 'T')
 SEPARATOR
-ENTRY("Alt + Q | Quit"          , quit         , MOD_ALT | MOD_NOREPEAT, 'X')
+ENTRY("Alt + Q | Quit"          , quit         , MOD_ALT | MOD_NOREPEAT, 'Q')
